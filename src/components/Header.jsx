a533fd4067fd5933e8e512184b1da627178187f9
Julienne H <juliennehattabi@gmail.com>
2024-06-24 10:27:32 +0200

    feat : make links functional in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import header from "../assets/Logo.png";
 import "../styles/main.scss";
+import { Link } from "react-router-dom";
 function Header() {
   return (
     <header>
@@ -8,8 +9,12 @@ function Header() {
         <img className="header_logo" src={header} alt="logo_header" />
       </div>
       <ul className="header_menu">
-        <li className="header_link">Accueil</li>
-        <li>A propos</li>
+        <li className="header_link">
+          <Link to="/home">Accueil</Link>
+        </li>
+        <li className="header_link">
+          <Link to="/about">A propos</Link>
+        </li>
       </ul>
     </header>
   );
