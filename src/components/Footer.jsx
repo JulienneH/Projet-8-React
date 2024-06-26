caec5041a3b2f986088c36f88d404acebde7ce3b
Julienne H <juliennehattabi@gmail.com>
2024-06-26 11:12:38 +0200

    feat : footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import footer from "../assets/Footer.png";
+import Footer from "../assets/logoFooter.png";
 import "../styles/main.scss";
-function Footer() {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright } from "@fortawesome/free-regular-svg-icons";
+
+function FooterComponent() {
   return (
     <footer>
-      <div>
-        <img className="footer" src={footer} alt="image_footer" />
+      <div className="footer">
+        <img className="footer_img" src={Footer} alt="logo" />
+        <FontAwesomeIcon className="footer_icon" icon={faCopyright} />
+        <h3 className="footer_text">2020 Kasa.All rights reserved</h3>
       </div>
     </footer>
   );
 }
 
-export default Footer;
+export default FooterComponent;
