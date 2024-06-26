b651e3c8776b927f8fa703ada991f4bb253d9477
Julienne H <juliennehattabi@gmail.com>
2024-06-26 10:43:17 +0200

    feat : link underline based on the page the user is on

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import header from "../assets/Logo.png";
-import Imageheader from "../assets/Imageheader.png";
 import "../styles/main.scss";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 function Header() {
+  const location = useLocation();
   return (
     <header>
       <div>
         <img className="header_logo" src={header} alt="logo_header" />
-        <img className="header_image" src={Imageheader} alt="Image_header" />
       </div>
       <ul className="header_menu">
-        <li className="header_link">
-          <Link to="/home">Accueil</Link>
+        <li
+          className={`header_link ${location.pathname === "/home" ? "active" : ""}`}
+        >
+          <Link to="/home" className="header_link_style">
+            Accueil
+          </Link>
         </li>
-        <li className="header_link">
-          <Link to="/about">A propos</Link>
+        <li
+          className={`header_link ${location.pathname === "/about" ? "active" : ""}`}
+        >
+          <Link to="/about" className="header_link_style">
+            A propos
+          </Link>
         </li>
       </ul>
     </header>
