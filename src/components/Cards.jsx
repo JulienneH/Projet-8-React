55a4bf087207518b9d1f4242d23279c9412091b0
Julienne H <juliennehattabi@gmail.com>
2024-06-26 11:52:51 +0200

    feat : recovery of json data and display in the home page

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import "../styles/main.scss";
+import data from "../data.json";
 
 const CardContainer = ({ children }) => {
   return <div className="card_container">{children}</div>;
 };
 
-const Card = ({ title }) => {
+const Card = ({ apartment }) => {
   return (
     <div className="card_individual">
       <div className="card_title">
-        <h1>{title}</h1>
+        <h1>{apartment.title}</h1>
       </div>
+      <img className="card_img" src={apartment.cover} alt={apartment.title} />
     </div>
   );
 };
 
-export default Card;
-export { CardContainer };
+function Cards() {
+  return (
+    <CardContainer>
+      {data.map((apartment) => (
+        <Card key={apartment.id} apartment={apartment} />
+      ))}
+    </CardContainer>
+  );
+}
+
+export default Cards;
+export { CardContainer, Cards };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/main.scss";
-import Card, { CardContainer } from "../components/Cards";
+import Cards from "../components/Cards";
 import coverHome from "../assets/coverHome.png";
 
 function Container() {
@@ -11,20 +11,7 @@ function Container() {
         <span className="home_text">Chez vous, partout et ailleurs</span>
       </div>
 
-      <CardContainer>
-        <Card title="Card 1" />
-        <Card title="Card 2" />
-        <Card title="Card 3" />
-        <Card title="Card 4" />
-        <Card title="Card 5" />
-        <Card title="Card 6" />
-        <Card title="Card 7" />
-        <Card title="Card 8" />
-        <Card title="Card 9" />
-        <Card title="Card 10" />
-        <Card title="Card 11" />
-        <Card title="Card 12" />
-      </CardContainer>
+      <Cards />
     </>
   );
 }
