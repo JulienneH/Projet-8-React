5aebf42bd8865af2c3f2d8e1ff7ce0f75c213e48
Julienne H <juliennehattabi@gmail.com>
2024-07-01 11:31:35 +0200

    feat : layout error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "../styles/main.scss";
+import { Link } from "react-router-dom";
 function Error() {
   return (
-    <div>
-      <h1 className="error_message">Désolé, cette page n'existe pas !</h1>
+    <div className="error">
+      <h1 className="error_404">404</h1>
+      <h2 className="error_message">
+        Oups ! La page que vous demandez n'existe pas.
+      </h2>
+      <Link to="/home" className="error_link">
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   );
 }
