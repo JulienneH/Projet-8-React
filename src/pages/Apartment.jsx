9e53c33fe6a4599cbb4e5c357c33399387e37fdc
Julienne H <juliennehattabi@gmail.com>
2024-07-09 11:53:32 +0200

    feat : importing apartment page components into the parent Apartment.jsx component

--- src/pages/Apartment.jsx
+++ src/pages/Apartment.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import data from "../data.json";
 import Caroussel from "../components/Caroussel";
+import Description from "../components/DescriptionApartment";
+import CollapseApartment from "../components/CollapseApartment";
+import Host from "../components/Host";
+import Tags from "..//components/Tags";
+import Rating from "..//components/Rating";
 
 function Apartment() {
   //extraction de l'identifiant de l'appart à partir de l'URL grace à useParams
@@ -15,9 +20,23 @@ function Apartment() {
 
   return (
     <div className="caroussel">
-      <Caroussel pictures={apartment.pictures} />
-      <h1 className="caroussel_title">{apartment.title}</h1>
-      <p className="caroussel_description">{apartment.description}</p>
+      <Caroussel
+        className="caroussel_container"
+        pictures={apartment.pictures}
+      />
+      <Description title={apartment.title} location={apartment.location} />
+      <CollapseApartment
+        className="caroussel_description"
+        description={apartment.description}
+        equipments={apartment.equipments}
+      />
+      <Host
+        className="host"
+        name={apartment.host.name}
+        picture={apartment.host.picture}
+      />
+      <Tags className="Tags" tags={apartment.tags} />
+      <Rating rating={apartment.rating} />
     </div>
   );
 }
